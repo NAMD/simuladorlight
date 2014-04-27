c89debe289b4d59c1e22b7a9042fcac37bcb85a8
Flavio C. Coelho <fccoelho@gmail.com>
2014-04-27 16:35:55 -0300

    Simulacao testada e funcionando para alocação aleatoria de notas

--- Interface/Simulador.py
+++ Interface/Simulador.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-
+import numpy as np
 
 
 def carrega_dados():
@@ -10,7 +10,7 @@ def carrega_dados():
     Lê os dados brutos.
     :rtype : pd.DataFrame
     """
-    df = pd.read_csv('tabelaparaosimulador.csv.gz',
+    df = pd.read_csv('Interface/tabelaparaosimulador.csv.gz',
                      names=["municipio", "contrato", "dt_inicio", "dt_fim", "data_processo", "data_corte",
                             "data_toi", "data_reclamacao", "data_negativacao"],
                      index_col=0, parse_dates=True, compression='gzip'
@@ -24,14 +24,28 @@ def calcula_prevalencias(dados):
     :rtype : tuple
     :param dados: Dataframe com os dados produzida por `carrega_dados`
     """
+    # Binariza os dados
     for n in dados.columns:
         if n.startswith("data_") or n == "contrato":
             dados[n] = pd.notnull(dados[n])
             dados[n].astype(int)
+    # cria colunas para os termos de interação
+    dados["_toicorte"] = dados.data_toi & dados.data_corte
+    dados["_toineg"] = dados.data_toi & dados.data_negativacao
+    dados["_toirec"] = dados.data_toi & dados.data_reclamacao
+    dados["_corteneg"] = dados.data_corte & dados.data_negativacao
+    dados["_corterec"] = dados.data_corte & dados.data_reclamacao
+    dados["_negrec"] = dados.data_negativacao & dados.data_reclamacao
+    dados["_toicorteneg"] = dados.data_toi & dados.data_corte & dados.data_negativacao
+    dados["_toicorterec"] = dados.data_toi & dados.data_corte & dados.data_reclamacao
+    dados["_toinegrec"] = dados.data_toi & dados.data_negativacao & dados.data_reclamacao
+    dados["_cortenegrec"] = dados.data_corte & dados.data_negativacao & dados.data_reclamacao
 
     del dados['dt_inicio'], dados['dt_fim']
     totais = dados.groupby(level=0).sum()
+    # Adicionando novas colunas
     totais["sem_nota"] = totais.contrato - (totais.data_corte + totais.data_toi + totais.data_negativacao + totais.data_reclamacao)
+
     municipios = totais.index.tolist()
     prevalencias = pd.DataFrame(index=municipios)
     for n in totais.columns:
@@ -49,23 +63,44 @@ def carrega_parametros():
     Lê os parâmetros da análise de sobrevivência, e retorna um dataframe indexado por município.
     :rtype : pd.DataFrame
     """
-    parametros = pd.read_csv("alphas.csv", index_col=1, header=0)
+    parametros = pd.read_csv("Interface/alphas.csv", index_col=1, header=0)
     #print(parametros[:10])
     return parametros
 
 
 def distribui_notas(ncorte, nneg, ntoi, processos):
     """
-    Distribui Notas de acordo com suas prevalências
+    Distribui Notas de acordo com suas prevalências nos municípios
     :param ncorte: Numero total de notas de corte
     :param nneg: Numero total de notas de negativação
     :param ntoi: Numero total de notas de toi
     :param processos: DataFrame com indice de municípios e seus geocódigos
     :return: Dataframe com as notas distribuídas
     """
+    # Primeiro calculo o tcn pois não precisa descontar nada
+    processos["tcn_apl"] = (ntoi+ncorte+nneg) * PREVALENCIAS.toicorteneg
+    # no calculo do número de só TN temos que descontar tcn que é contado duas vezes
+    processos["sotn_apl"] = (ntoi+nneg) * PREVALENCIAS.toineg - processos.tcn_apl
+    # no calculo do número de só TC temos que descontar tcn que é contado duas vezes
+    processos["sotc_apl"] = (ntoi+ncorte) * PREVALENCIAS.toicorte - processos.tcn_apl
+    # no calculo do número de só CN temos que descontar tcn que é contado duas vezes
+    processos["socn_apl"] = (ncorte+nneg) * PREVALENCIAS.corteneg
+    # No cálculo de só Toi precisamos descontar só toicorte, só toineg e tcn
+    processos["sotois_apl"] = ntoi * PREVALENCIAS.toi - (processos.sotc_apl+processos.sotn_apl+processos.tcn_apl)
+    # No cálculo de só Toi precisamos descontar só toicorte, só toineg e tcn
+    processos["socortes_apl"] = ncorte * PREVALENCIAS.corte - (processos.sotc_apl+processos.socn_apl+processos.tcn_apl)
+    # No cálculo de só Neg precisamos descontar só corteneg, só toineg e tcn
+    processos["sonegs_apl"] = nneg * PREVALENCIAS.negativacao - (processos.sotn_apl+processos.socn_apl+processos.tcn_apl)
+
     processos["tois_apl"] = ntoi * PREVALENCIAS.toi
     processos["cortes_apl"] = ncorte * PREVALENCIAS.corte
     processos["negs_apl"] = nneg * PREVALENCIAS.negativacao
+    processos["tc_apl"] = (ntoi+ncorte) * PREVALENCIAS.toicorte
+    processos["tn_apl"] = (ntoi+nneg) * PREVALENCIAS.toineg
+    #processos["tr_apl"] = (ntoi+nrec)
+    processos["cn_apl"] = (ncorte+nneg) * PREVALENCIAS.corteneg
+
+
     return processos
 
 
@@ -80,13 +115,17 @@ def simula(horizonte=30, ntoi=0, ncorte=0, nneg=0):
     """
     processos = pd.DataFrame(index=PARAMETROS.index.tolist())
     processos['geocodigo'] = PARAMETROS.geocodigo
-    total_notas = ntoi + ncorte + nneg
+
     # Distribuindo as notas por municipio de acordo com as prevalencias
     processos = distribui_notas(ncorte, nneg, ntoi, processos)
+    total_notas = processos.tois_apl+processos.cortes_apl+processos.negs_apl
     processos['notas_total'] = processos.tois_apl + processos.cortes_apl + processos.negs_apl
-    processos['Novos'] = horizonte * (PARAMETROS.alfa0*processos.notas_total + PARAMETROS.c*processos.cortes_apl +\
-                                      PARAMETROS.t*processos.tois_apl + PARAMETROS.n*processos.negs_apl)
+    processos['Novos'] = total_notas-total_notas*np.exp(-horizonte * (PARAMETROS.alfa0 + PARAMETROS.c*processos.cortes_apl +\
+                                      PARAMETROS.t*processos.tois_apl + PARAMETROS.n*processos.negs_apl + \
+                                      PARAMETROS.tc*processos.tc_apl + PARAMETROS.tn*processos.tn_apl + \
+                                      PARAMETROS.cn*processos.cn_apl + PARAMETROS.tcn*processos.tcn_apl))
     print(processos)
+    print(processos.Novos.sum())
     return processos
 
 
--- Interface/tests.py
+++ Interface/tests.py
@@ -1,6 +1,6 @@
 #from django.test import TestCase
 import unittest
-from Interface.Simulador import carrega_parametros, calcula_prevalencias, carrega_dados
+from Interface.Simulador import carrega_parametros, calcula_prevalencias, carrega_dados, simula
 
 
 class TestaSimulador(unittest.TestCase):
@@ -20,6 +20,20 @@ class TestaSimulador(unittest.TestCase):
         self.assertAlmostEqual(prev.toi.sum(), 1, 3)
         self.assertAlmostEqual(prev.negativacao.sum(), 1, 3)
         self.assertAlmostEqual(prev.reclamacao.sum(), 1, 3)
+        self.assertAlmostEqual(prev.toicorte.sum(), 1, 3)
+        self.assertAlmostEqual(prev.toineg.sum(), 1, 3)
+
+    def testa_soma_notas_isoladas(self):
+        processos = simula(90, 1000, 1000, 1000)
+        self.assertAlmostEqual(processos.tois_apl.sum(), 1000)
+        self.assertAlmostEqual(processos.cortes_apl.sum(), 1000)
+        self.assertAlmostEqual(processos.negs_apl.sum(), 1000)
+        self.assertAlmostEqual(processos.sotois_apl.sum()+processos.sotn_apl.sum()+processos.sotc_apl.sum()+processos.tcn_apl.sum(), processos.tois_apl.sum())
+        self.assertAlmostEqual(processos.socortes_apl.sum()+processos.socn_apl.sum()+processos.sotc_apl.sum()+processos.tcn_apl.sum(), processos.cortes_apl.sum())
+        self.assertAlmostEqual(processos.sonegs_apl.sum()+processos.socn_apl.sum()+processos.sotn_apl.sum()+processos.tcn_apl.sum(), processos.negs_apl.sum())
+       # self.assertAlmostEqual(processos.sotois_apl.sum()+processos.socortes_apl.sum()+processos.sotc_apl.sum()+\
+       #                        processos.sonegs_apl.sum()+processos.sotn_apl.sum()+processos.tcn_apl.sum()+\
+       #                        processos.socn_apl.sum(), processos.tois_apl.sum()+processos.cortes_apl.sum()+processos.negs_apl.sum(), 1)
 
 
 
