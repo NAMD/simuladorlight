47fdbefe6f741cb9eaf2f6c44559c1e2de9b6bce
Flávio Codeço Coelho <fccoelho@gmail.com>
2014-04-28 08:40:18 -0300

    Adicionei suporte inicial para análises a nível de município

--- Interface/views.py
+++ Interface/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView, View
 from django.contrib import messages
 import pandas as pd
@@ -13,7 +14,10 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
         messages.info(self.request, 'Bem Vindo ao Simulador de Processos Judiciais.')
-
+        horizonte = 60
+        toi = 100
+        corte = 100
+        neg = 100
         try:
             toi = int(self.request.GET['toi'])
             corte = int(self.request.GET['corte'])
@@ -22,9 +26,25 @@ class HomePageView(TemplateView):
             processos = Simulador.simula(horizonte, toi, corte, neg)
         except KeyError:
             messages.error(self.request, "Todos os campos devem ser preenchidos")
-            processos = json.dumps({})
+            processos = Simulador.simula(horizonte, toi, corte, neg)
         processos = processos.set_index("geocodigo")
-        context.update({"processos": processos.Novos.to_json() if isinstance(processos, pd.DataFrame) else json.dumps({})
+        context.update({"processos": processos.Novos.to_json() if isinstance(processos,
+                                                                             pd.DataFrame) else json.dumps({}),
+                        'horizonte': horizonte,
+                        'toi': toi,
+                        'corte': corte,
+                        'neg': neg,
+
+        })
+        return context
+
+class LocalAnalysisView(TemplateView):
+    template_name = 'local.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(LocalAnalysisView)
+
+        context.update({
 
         })
         return context
--- Simulador_Light/urls.py
+++ Simulador_Light/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
-from Interface.views import HomePageView
+from Interface.views import HomePageView, LocalAnalysisView
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^local/', LocalAnalysisView.as_view(), name='local'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
